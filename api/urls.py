1a963125ac50cc864e557df4f3473e0e3f8aacdb
scode97 <sehameldeen1997@gmail.com>
2018-09-09 20:42:40 +0300

    no change

--- api/urls.py
+++ api/urls.py
@@ -7,9 +7,7 @@ from api.views import PlansList
 urlpatterns = [
     path('login/', obtain_jwt_token, name='login'),
     path('register/', UserCreateAPIView.as_view(), name='register'),
-    # path('api/planList/', ListView.as_view(), name='api-list'),
-    # path('api/mealTypeList/', MealTypeView.as_view(), name='api-TypeList'),
+
     path('plansList/', PlansList.as_view(), name='api-plan-List'),
-    # path('api/snacksList/', SnacksList.as_view(), name='api-snack-List'),
 
 ]
--- api/views.py
+++ api/views.py
@@ -6,20 +6,9 @@ class UserCreateAPIView(CreateAPIView):
     serializer_class = UserCreateSerializer
 
 
-# class ListView(ListAPIView):
-# 	queryset = PlanList.objects.all()
-# 	serializer_class = PlanListSerializer
-
-# class MealTypeView(ListAPIView):
-# 	queryset = MealTypeList.objects.all()
-# 	serializer_class = MealListSerializer
-
 class PlansList(ListAPIView):
 	queryset = Plans.objects.all()
 	serializer_class = PlansSerializer
 
-# class SnacksList(ListAPIView):
-# 	queryset = ListOfSnack.objects.all()
-# 	serializer_class = ListOfSnacksSerializer
 
 
